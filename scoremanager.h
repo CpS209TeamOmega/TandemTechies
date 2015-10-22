65fc86f224d752fb413be8b3fa21c88e55672e87
zhe480 <zhe480@bobjonesacademy.net>
2015-10-22 19:43:24 -0400

    score manager updated

--- scoremanager.h
+++ scoremanager.h
@@ -8,9 +8,21 @@
 
 class ScoreManager
 {
-public:
-    ScoreManager();
+private:
+    QSqlDatabase db;
+    QHash<string, int> dashBoard;
+    QHash<string, int> scoreTable;
+    int hiScore;
 
+public:
+    ScoreManager();//create database, set everything, check open
+
+    int getHiScore();//return the highest score ever
+    QHash<string, int> getTable();//return the records of scores and players(hashtable)
+
+    bool addScore(string player, int score);//Add to scoreTable and update dashBoard
+
+    bool updateDash();//called whenever addScore is called
 };
 
 #endif // SCOREMANAGER_H
