3a2fe9d2dfd680b3b1eda1df22ed1e9bcad9efd4
shenr124 <shenr124@students.bju.edu>
2015-10-22 18:59:44 -0400

    Created basic Entity class

--- entity.h
+++ entity.h
@@ -1,10 +1,31 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <QRect>
+
 class Entity
 {
+    QRect rect;
+
 public:
     Entity();
+
+    bool isCollidingWith(Entity& otherEntity);
+
+    void update() = 0;
+
+    //Getters
+    int getX() { return rect.x(); }
+    int getY() { return rect.y(); }
+    int getWidth() { return rect.width(); }
+    int getHeight() { return rect.height(); }
+    QRect getRect() { return rect; }
+
+    //Setters
+    void setX(int newX) { x = newX; }
+    void setY(int newY) { y = newY; }
+    void setWidth(int newWidth) { width = newWidth; }
+    void setHeight(int newHeight) { height = newHeight; }
 };
 
 #endif // ENTITY_H
